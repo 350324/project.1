6f0b3c75ad17ba4ae18ee68f32b74c80cdbc2cb9
350324 <3856373922@qq.com>
2026-09-15 18:34:17 +0800

    Add files via upload

---
+++ 视频2笔记.cpp
@@ -0,0 +1,161 @@
+#include "stdio.h" 
+
+
+
+int main()
+{
+    a=b+c;//语句 
+
+   return 0;
+
+
+}
+
+int a-2
+int b-3
+intc=4
+
+    a=b-2;
+    	a=b=c 
+    printf("a=%d\nb=%d",a,b);//只有出现在等号左边才会被修改 
+	 
+    a+=2//等价于a=a+2 
+    
+
+
+
+
+    12=a;
+     int main()     //设置一个c程序，输入数字，由此【判断奇偶 
+	{
+	
+		scanf("%d,&num");
+	if(num%2==0)	
+		printf(这是一个偶数“)； 
+		eles
+		printf(这是一个奇数") ； 
+		
+		
+		return 0;
+		
+		
+		
+		
+		//级联式if语句 
+		
+		
+		//输入其他分数显示成绩输入有误 
+		int main()
+			printf(请输入你的成绩：");
+			scanf("%d,&num");
+		f(num%2==0)	
+		{
+		Printf("您的成绩不及格")； 
+		}
+		eles if (num=1)
+			Printf("您的成绩不及格")；
+		
+			eles if (num=1)
+		{
+		}
+			Printf("您的成绩不及格")；
+			eles if (num=2)
+		{	Printf("您的成绩不及格")；
+		 } 
+		
+			eles if (num=3)
+		{
+		}	Printf("您的成绩不及格")；
+		
+			eles if (num=4)
+		{	Printf("您的成绩不及格")；
+		}
+		
+			eles if (num=5)
+		{
+		}	Printf("您的成绩不及格")；
+		
+			eles if (num=6)
+		{
+		 } 	Printf("您的成绩及格")；
+		
+			eles if (num=7)
+		{
+		}	Printf("您的成绩优秀")；
+		
+			eles if (num=8)
+		{	Printf("您的成绩优秀")；
+		
+			eles if (num=9)
+		{	Printf("您的成绩优秀")；
+		
+		} 
+		
+			eles if (num=10)
+		{	Printf("您的成绩满分")；
+		eles 
+		Printf("您的成绩输入有误")；
+		
+		
+		 } 
+		 ///条件表达句子 
+		    /// if/eles 
+		    
+		   /// 表达式1为真 选则表达式22 否则选3 
+	   if(1){
+	    printf (”这是真的")
+	    eles
+	    {
+	    	 printf (”这是假的")
+	    	
+		}
+		switch(表达式) 
+		///case常量表达式;语句 
+		
+		
+		
+		
+		case0
+			Printf("您的成绩不合格")；
+		case1
+		
+			Printf("您的成绩不合格")；
+		case2
+			Printf("您的成绩不合格")；
+		case3
+		
+			Printf("您的成绩不合格")；
+		case4
+		
+			Printf("您的成绩不合格")；
+		case5
+		
+			Printf("您的成绩不合格")；
+		case6
+			Printf("您的成绩合格")；
+		case7
+			Printf("您的成绩优秀")；
+		caes8
+			Printf("您的成绩优秀")；
+		case 9
+	}	Printf("您的成绩优秀")；
+	    caes10
+		Printf("您的成绩满分")；
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
