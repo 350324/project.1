e351709cc225e05464f82e933e11e9c6d860bee2
350324 <3856373922@qq.com>
2026-09-16 18:56:04 +0800

    Add files via upload

---
+++ 视频三学习笔记.cpp
@@ -0,0 +1,110 @@
+///while 语句的基本结构 
+//while(表达式)
+
+
+
+   #include  "stdio.h"\
+   
+   int i=5;
+   
+     int main()
+   {
+        while(5)
+   	  {
+   		printf("%d\n",i);
+   	
+	   
+	   
+	   
+	   	i=i-1;
+		   
+   		
+   	 
+	   }
+   	printf("循环执行完了" );
+   	
+   	//i-- &  --i区别 
+   	
+   	
+   	
+   	return 0;
+   }
+   
+
+   //do语句，while表达式 
+   
+   int num;
+   {
+   	
+         printf("请输入一个整数：");
+  scanf("%d",&num);
+   
+   	
+     do 
+	   
+	{
+	   num  =num/10;    ///num/=10; 
+	   i++;  
+	   
+}
+	   printf("输入的数字有%d"，i)
+	   
+	   
+	   }
+	} 
+		return 0;
+	
+	
+   ///for语句 for(表达式1  ；表达式2：表达式3) 语句 
+   
+   
+   
+   
+   
+   
+   
+      printf("请输入一个整数：");
+  scanf("%d",&num);
+   
+   int main() {
+   
+   for(i=5,i>0  I--){
+   }//语句 
+   
+   
+   
+   //等价于
+   i=5; 
+   
+   
+   while(i>0)
+   {
+   //语句 
+   
+   printf"%d\n",i);
+   
+   ///break 回跳转到这里
+   
+   return 0 ; 
+   
+   } 
+   printf("请输入一个整数：")
+     scanf("%d",&num);
+   for(i=2 i<9;i++) 
+   
+   if(   (9%)   ==
+   break;
+   //break跳转回到循环体的末尾之后执行
+   
+   printf("1")
+    printf("2")
+    continue
+    printf("3)
+    printf("4)
+   //continue语句跳转到这里
+   
+   
+   
+   
+   
+   
