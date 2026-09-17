cbbeb33a34070572ca883d7d63492d42c164befe
350324 <3856373922@qq.com>
2026-09-17 22:13:15 +0800

    Add files via upload

---
+++ 视频四学习笔记9.17.cpp
@@ -0,0 +1,103 @@
+//signed 为强调可以省略
+
+# include  <stdio.h>
+
+
+
+char  zimu='103';
+
+
+   
+
+
+
+
+int main() 
+{
+	
+	 printf("%c",zimu);
+	
+	
+	
+	
+	
+	return 0;
+}
+ 
+ 
+ # include  <stdio.h>
+ 
+     float a=0.1f;
+ 
+ int main ()    //浮点数精度优先  
+ {
+ printf("%f ,a");
+ 	        ///float比较中，谨慎使用==   !== 
+ 	       //使用基于误差范围的方法 
+ 	
+ 	
+ 	
+ 	if (a==0.1f)
+ 	
+ 	
+ 	
+ 	return 0;
+ 	
+ }
+ 
+ #define u8 unsigned char      ///define的运用 
+ #define u32 unsigned int   
+ 
+ 
+ u8a;
+ u32 b;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
