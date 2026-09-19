1ba86e1f495de4a641b211057fc71ab3ee42f33b
350324 <3856373922@qq.com>
2026-09-19 18:51:46 +0800

    Add files via upload

---
+++ 视频五学习笔记9.19.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+
+int student1 =45;
+int student2=56;
+int student3=67;
+
+int  num;
+int main()
+
+
+{
+	
+	printf( "请输入学员的学号");
+	
+	scanf("%d",&num); 
+
+
+   switch  (num)
+	
+	{
+	case1:
+	     printf("学员1的成绩为:%d",student1);
+	break;
+	
+	case2:
+	     printf("学员2的成绩为:%d",student2);
+	break;
+	
+	case3:
+	    printf("学员3的成绩为:%d",student3);
+	break;
+	
+	
+	}
+	
+
+	
+	return  0;
+}
+
+
+#include <stdio.h>
+
+int student [3]={0}
+int  i;
+int j;
+
+
+int main ()
+{
+	for(i=0;i<3;I++)
+	
+	
+printf("请输入学员的学号:")
+	scanf("%",&)
+	printf("请输入学员的成绩:“);
+		scanf("%",&student{i-1`};
+	
+	
+	 printf("请输入学员的学号:")
+	
+	scanf("%d"&j);
+	printf("%d的成绩为;%d",j,student[j-1])
+	
+	
+	
+	
+	输请学员的成绩”:)
+	
+return  0;
+	
+}
+
+
+                 ///CONST 关键字，在代码运行过程中不会被修改
+                 
+                 
+            #include <stdio.h>
+            
+            int a=1
+            int*  p=NULL; 
+            
+            int main()
+            {
+            	p=&a;
+            	
+            	
+            	*p=5;
+            	printf("%d",a);
+            	return 0;
+            	
+            	
+			}
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+
